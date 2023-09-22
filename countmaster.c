168990bd5af3e5fc9127984f9076d7f5854c36d5
TheMarksmen <TheMarksmen911@gmail.com>
2023-09-22 15:59:17 -0400

    minor change

--- countmaster.c
+++ countmaster.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 int main() {
+
     char* arguments[4];			//make an array that can hold up to 4 strings
     arguments[0]="./countprimes";	//the first string is the command name
     arguments[1]="10";			//the second and third strings are the start and end parameters
