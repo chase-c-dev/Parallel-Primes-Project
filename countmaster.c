81a80b686081ec53ff0ef7717e637afd11a6735c
RockStoneRock-droid <chasegameac@gmail.com>
2023-09-24 17:26:23 -0400

    Added fork to countmaster

--- countmaster.c
+++ countmaster.c
@@ -1,13 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
 
 
-int main() {
-
-    char* arguments[4];			//make an array that can hold up to 4 strings
-    arguments[0]="./countprimes";	//the first string is the command name
-    arguments[1]="10";			//the second and third strings are the start and end parameters
-    arguments[2]="30";
-    arguments[3]=NULL;			//the last argument must be a null terminator
-    execvp("./countprimes",arguments);	//call the countprimes child program
+int main(){
+	pid_t processID = fork();
+	
+	if (processID == 0) {
+		char* arguments[4];
+		arguments[0]="./countprimes";
+		arguments[1]="10";
+		arguments[2]="30";
+		arguments[3]=NULL;
+		execvp("./countprimes",arguments);
+	}
+	
+	else {
+		int currentstat;
+		waitpid(processID, &currentstat, 0);
+		printf("test success");
+	}
+	return 0;
 }
