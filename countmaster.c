b5d50b940606f9a08184466bd031d923eb75a7e4
RockStoneRock-droid <chasegameac@gmail.com>
2023-09-25 17:31:52 -0400

    Altered to run with a loop
    runs with a loop 4 times, running countprimes 4 times

--- countmaster.c
+++ countmaster.c
@@ -4,22 +4,30 @@
 #include <sys/types.h>
 
 
-int main(){
-	pid_t processID = fork();
+int main(int argc, char *argv[]) {
+	int NUMBER_OF_CALLS = atoi(argv[3]);
+	char *value1 = argv[1];
+	char *value2 = argv[2];
+	for (int i = 0; i < NUMBER_OF_CALLS; i++) {
+		pid_t processID = fork();
+		if (processID == 0) {
+		//	char* arguments[4];
+		//	arguments[0]="./countprimes";
+		//	arguments[1]="10";
+		//	arguments[2]="30";
+		//	arguments[3]=NULL;
+		//	char *args[] = {value1, value2, NULL};
+			execvp("./countprimes", argv);
+		//	execvp("./countprimes",arguments);
+		}
 	
-	if (processID == 0) {
-		char* arguments[4];
-		arguments[0]="./countprimes";
-		arguments[1]="10";
-		arguments[2]="30";
-		arguments[3]=NULL;
-		execvp("./countprimes",arguments);
-	}
-	
-	else {
-		int currentstat;
-		waitpid(processID, &currentstat, 0);
-		printf("test success");
+		else {
+			int currentstat;
+			waitpid(processID, &currentstat, 0);
+
+			int  res = WEXITSTATUS(currentstat);
+		//	printf("The  countprimes number is %d\n", res);
+		}
 	}
 	return 0;
 }
