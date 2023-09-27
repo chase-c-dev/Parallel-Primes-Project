77172745f312d3cb9a93aa5a7e84d6f61d41580d
RockStoneRock-droid <chasegameac@gmail.com>
2023-09-26 21:24:08 -0400

    Updated to alter every iteration
    Changes execvp run every iteration, final step of project

--- countmaster.c
+++ countmaster.c
@@ -8,26 +8,32 @@ int main(int argc, char *argv[]) {
 	int NUMBER_OF_CALLS = atoi(argv[3]);
 	char *value1 = argv[1];
 	char *value2 = argv[2];
-	for (int i = 0; i < NUMBER_OF_CALLS; i++) {
+	int allids[NUMBER_OF_CALLS];
+	int total = 0;
+	int temp = 0;
+	sprintf(argv[2], "%d", atoi(argv[2])/2);
+	for (int i = 0; i < NUMBER_OF_CALLS*5; i+=5) {
 		pid_t processID = fork();
 		if (processID == 0) {
-		//	char* arguments[4];
-		//	arguments[0]="./countprimes";
-		//	arguments[1]="10";
-		//	arguments[2]="30";
-		//	arguments[3]=NULL;
-		//	char *args[] = {value1, value2, NULL};
+//			int new_low_counter = high_range - low_range + 1;
+//			low_range += new_low_counter;
+//			high_range += 5;
+			sprintf(argv[1], "%d", atoi(argv[1]) + i);
+			sprintf(argv[2], "%d", atoi(argv[2]) + i);
+		//	argv[1] = 
+		//	argv[2] = arg2s[i];
 			execvp("./countprimes", argv);
-		//	execvp("./countprimes",arguments);
-		}
-	
-		else {
-			int currentstat;
-			waitpid(processID, &currentstat, 0);
-
-			int  res = WEXITSTATUS(currentstat);
-		//	printf("The  countprimes number is %d\n", res);
+		} else {
+			allids[temp] = processID;
+			temp++;
 		}
 	}
+	for (int j = 0; j < NUMBER_OF_CALLS; j++) {
+		int currentState;
+		waitpid(allids[j], &currentState, 0);
+		int res = WEXITSTATUS(currentState);
+		 total += res; 
+	}
+	printf("Total is %d\n", total);
 	return 0;
 }
